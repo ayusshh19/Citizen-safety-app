0ea6bc380ef380917c83a6ae31548bdf62dfbeed
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-02-28 21:49:09 +0530

    bottom tab navigation set with UI animation

--- citizensafetyapp/screens/Homescreen.js
+++ citizensafetyapp/screens/Homescreen.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, Dimensions, TouchableOpacity, PermissionsAndroid, DeviceEventEmitter, Alert, NativeEventEmitter } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Lottie from 'lottie-react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -7,22 +7,60 @@ import { removeItem } from '../utils/asyncStorage';
 const { width, height } = Dimensions.get('window');
 import SmsListener from 'react-native-android-sms-listener'
 import Contacts from 'react-native-contacts';
-import  {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Mainpage from './Mainpage';
 import Sos from './Sos';
 import Others from './Others';
 import Infopage from './Infopage';
+import Colors from '../redux/constants/colors';
 import Blog from './Blog';
+import Icon, { Icons } from '../components/Icons';
+import * as Animatable from 'react-native-animatable';
+const TabArr = [
+  { route: 'Home', label: 'Home', type: Icons.Ionicons, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Mainpage },
+  { route: 'Sos', label: 'Like', type: Icons.MaterialCommunityIcons, activeIcon: 'heart-plus', inActiveIcon: 'heart-plus-outline', component: Sos },
+  { route: 'Info', label: 'Search', type: Icons.MaterialCommunityIcons, activeIcon: 'timeline-plus', inActiveIcon: 'timeline-plus-outline', component: Infopage },
+  { route: 'Blog', label: 'Account', type: Icons.FontAwesome, activeIcon: 'user-circle', inActiveIcon: 'user-circle-o', component: Blog },
+  { route: 'Others', label: 'Account', type: Icons.FontAwesome, activeIcon: 'user-circle', inActiveIcon: 'user-circle-o', component: Others },
+];
 
-const Tab = createBottomTabNavigator()
 
+const Tab = createBottomTabNavigator();
+
+const TabButton = (props) => {
+  const { item, onPress, accessibilityState } = props;
+  const focused = accessibilityState.selected;
+  const viewRef = useRef(null);
+
+  useEffect(() => {
+    if (focused) {
+      viewRef.current.animate({ 0: { scale: .5, rotate: '0deg' }, 1: { scale: 1.5, rotate: '360deg' } });
+    } else {
+      viewRef.current.animate({ 0: { scale: 1.5, rotate: '360deg' }, 1: { scale: 1, rotate: '0deg' } });
+    }
+  }, [focused])
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={1}
+      style={[styles.container, { top: 0 }]}>
+      <Animatable.View
+        ref={viewRef}
+        duration={1000}
+      >
+        <Icon type={item.type}
+          name={focused ? item.activeIcon : item.inActiveIcon}
+          color={focused ? Colors.primary : Colors.gradientForm} />
+      </Animatable.View>
+      <Animatable.Text
+      style={[styles.text, Colors.black ]}>
+          {item.label}
+        </Animatable.Text>
+    </TouchableOpacity>
+  )
+}
 export default function HomeScreen() {
   const navigation = useNavigation();
-  const eventEmitter = new NativeEventEmitter();
-  const handleReset = async () => {
-    await removeItem('onboarded');
-    navigation.push('Onboarding');
-  }
 
   const [receiveSmsPermission, setReceiveSmsPermission] = useState('');
 
@@ -35,7 +73,7 @@ export default function HomeScreen() {
           PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
           PermissionsAndroid.PERMISSIONS.RECEIVE_SMS]
         );
-        console.log(permission)
+      console.log(permission)
       setReceiveSmsPermission(permission);
     } catch (err) {
       console.log(err);
@@ -56,23 +94,23 @@ export default function HomeScreen() {
       title: 'Contacts',
       message: 'This app would like to view your contacts.',
       buttonPositive: 'Please accept bare mortal',
-  })
+    })
       .then((res) => {
-          console.log('Permission: ', res);
-          Contacts.getAll()
-              .then((contacts) => {
-                  // work with contacts
-                  const data = contacts.map((contact)=>{
-                    return contact.phoneNumbers
-                  })
-                  console.log(data)
-              })
-              .catch((e) => {
-                  console.log(e);
-              });
+        console.log('Permission: ', res);
+        Contacts.getAll()
+          .then((contacts) => {
+            // work with contacts
+            const data = contacts.map((contact) => {
+              return contact.phoneNumbers
+            })
+            // console.log(data)
+          })
+          .catch((e) => {
+            console.log(e);
+          });
       })
       .catch((error) => {
-          console.error('Permission error: ', error);
+        console.error('Permission error: ', error);
       });
     if (receiveSmsPermission === PermissionsAndroid.RESULTS.GRANTED) {
 
@@ -96,36 +134,47 @@ export default function HomeScreen() {
   }, [receiveSmsPermission]);
 
   return (
-    <Tab.Navigator  >
-      <Tab.Group
-      screenOptions={{headerShown:false,}}
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            height: 60,
+            position: 'absolute',
+            margin: 16,
+            borderRadius: 16,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }
+        }}
       >
-        <Tab.Screen name='homemain' component={Mainpage}/>
-        <Tab.Screen name='sos' component={Sos}/>
-        <Tab.Screen name='others' component={Others}/>
-        <Tab.Screen name='infopage' component={Infopage}/>
-        <Tab.Screen name='blog' component={Blog}/>
-      </Tab.Group>
-    </Tab.Navigator>
+        {TabArr.map((item, index) => {
+          return (
+            <Tab.Screen key={index} name={item.route} component={item.component}
+              options={{
+                tabBarShowLabel: false,
+                tabBarButton: (props) => <TabButton {...props} item={item} />
+              }}
+            />
+          )
+        })}
+      </Tab.Navigator>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
-  },
-  lottie: {
-    width: width * 0.9,
-    height: width
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
   },
   text: {
-    fontSize: width * 0.09,
-    marginBottom: 20
-  },
-  resetButton: {
-    backgroundColor: '#34d399',
-    padding: 10,
-    borderRadius: 10
+    fontSize: 12,
+    textAlign: 'center',
+    marginTop:5,
+    color: Colors.primary,
+    fontWeight: '500'
   }
 })
