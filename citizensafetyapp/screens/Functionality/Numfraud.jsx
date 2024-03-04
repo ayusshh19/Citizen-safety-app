109f4537b207c625e865df15de54d5d77c20fece
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-04 14:06:56 +0530

    Fraud calls UI set

--- citizensafetyapp/screens/Functionality/Numfraud.jsx
+++ citizensafetyapp/screens/Functionality/Numfraud.jsx
@@ -2,7 +2,7 @@ import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import colors from '../../redux/constants/colors';
 import CallLogs from 'react-native-call-log';
-import {ListItem, Avatar, Button} from '@rneui/themed';
+import {ListItem, Avatar, Button, Icon, Input} from '@rneui/themed';
 export default function Numfraud() {
   const [loadcall, setloadcall] = useState([]);
   useEffect(() => {
@@ -10,6 +10,26 @@ export default function Numfraud() {
   }, []);
   return (
     <View style={styles.component}>
+      <View style={styles.inputcomponent}>
+        <Input placeholder="Enter your number to test" keyboardType="number-pad" style={{color:"white"}} containerStyle={{
+                width: 300,
+              }}/>
+        <Button
+              title="Test Number"
+              buttonStyle={{
+                backgroundColor: colors.buttonbg,
+                borderRadius: 3,
+              }}
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 70,
+                marginVertical: 10,
+              }}
+            />
+      </View>
+      <Text style={{fontSize:30,marginBottom:20,fontWeight:"600",textAlign:"left"}}>
+        Call Logs
+      </Text>
       <ScrollView>
         {loadcall &&
           loadcall.map((data, index) => {
@@ -22,8 +42,8 @@ export default function Numfraud() {
                   }}
                   containerStyle={{
                     backgroundColor: colors.grayscreenbg,
-                    borderRadius:10,
-                    color:colors.white
+                    borderRadius: 10,
+                    color: colors.white,
                   }}
                   leftContent={reset => (
                     <Button
@@ -34,7 +54,7 @@ export default function Numfraud() {
                         minHeight: '90%',
                         minWidth: '90%',
                         margin: 3,
-                        borderRadius:10
+                        borderRadius: 10,
                       }}
                     />
                   )}
@@ -47,7 +67,7 @@ export default function Numfraud() {
                         backgroundColor: 'red',
                         minWidth: '90%',
                         margin: 3,
-                        borderRadius:10
+                        borderRadius: 10,
                       }}
                     />
                   )}>
@@ -60,7 +80,9 @@ export default function Numfraud() {
                     <ListItem.Title style={{color: 'white', width: 'auto'}}>
                       {data.name ? data.name : 'UNKNOWN'}
                     </ListItem.Title>
-                    <ListItem.Subtitle style={{color:colors.buttonbg}}>{data.phoneNumber}</ListItem.Subtitle>
+                    <ListItem.Subtitle style={{color: colors.buttonbg}}>
+                      {data.phoneNumber}
+                    </ListItem.Subtitle>
                   </ListItem.Content>
                   <ListItem.Chevron />
                 </ListItem.Swipeable>
@@ -81,4 +103,14 @@ const styles = StyleSheet.create({
   textstyles: {
     fontSize: 20,
   },
+  inputcomponent:{
+    width: '100%',
+    margin:20,
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  inputtest:{
+    width:"80%"
+  }
 });
